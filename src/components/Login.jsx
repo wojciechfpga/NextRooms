a56262f91d761025d564d9ecaecba9bb8e0da50f
wojciechfpga <lasowski.wojciech.poczta@gmail.com>
2024-12-10 20:28:57 +0100

    First

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,10 @@
 'use client';
 
-import AuthForm from "src/components/AuthForm";
+import AuthForm from "./AuthForm";
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import { useRouter } from 'next/navigation';
-import { loginUser, registerUser, logout } from "src/lib/features/auth/authSlice";
+import { loginUser } from "@/lib/features/auth/authSlice";
 
 const Login = () => {
   const dispatch = useDispatch();
